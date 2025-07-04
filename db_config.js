2e68a7c53c3592db1d673f979d40c38e644871d3
josalza902 <131299934+josalza902@users.noreply.github.com>
2025-07-04 13:45:07 -0500

    Add files via upload

---
+++ db_config.js
@@ -0,0 +1,143 @@
+use CentroMédicoAcme;
+db.createCollection("oficinas",{
+    validator:{
+        $jsonSchema:{
+            bsonType:"object",
+            required:["codigo","piso","activo"],
+            properties:{
+                codigo:{
+                    bsonType:"string"
+                
+                },
+                piso:{
+                    bsonType:"int"
+                                    },
+                activo:{
+                    bsonType:"bool"
+                },
+            }
+        }
+    }
+});
+db.createCollection("medicinas",{
+    validator:{
+        $jsonSchema:{
+            bsonType:"object",
+            required:["codigo","nombre","activo"],
+            properties:{
+                codigo:{
+                    bsonType:"string"
+                    
+                    
+                },
+                nombre:{
+                    bsonType:"string"
+                },
+                descripcion:{
+                    bsonType:"string"
+                    
+                },
+                activo:{
+                    bsonType:"bool"
+                }
+                
+            }
+        }
+    }
+});
+db.createCollection("personas",{
+    validator:{
+        $jsonSchema:{
+            required:["identificacion","primerNombre","apellido","email","numeroDeTelefono","tipoDeSangre","direccion"],
+            properties:{
+                identificacion:{
+                    bsonType:"string",
+                    pattern:"^[0-9]{12}$"
+                },
+                primerNombre:{
+                    bsonType:"string"  
+                },
+                apellido:{
+                    bsonType:"string"
+                },
+                email:{
+                    bsonType:"string"
+                    
+                },
+                numeroDeTelefono:{
+                    bsonType:"string",
+                    pattern:"^[0-9]{10}"
+                },
+                tipoDeSangre:{
+                    bsonType:"string",
+                    enum:["O+","O-","A+","A-","B+","B-"]
+                },
+                direcciones:{
+                    bsonType:"string"
+                    
+                }
+            }
+        }
+    }
+});
+db.createCollection("doctores",{
+    validator:{
+        $jsonSchema:{
+            bsonType:"object",
+            required:["persona_id","codigo","especializacion","fechaDeInicio","activo"],
+            properties:{
+                codigo:{
+                    bsonType:"int",
+                },
+                especialidad:{
+                    bsonType:"string",
+                },
+                fechaDeInicio:{
+                    bsonType:"date"
+                },
+                activo:{
+                    bsonType:"bool",
+                }
+            }
+        }
+    }
+});
+db.createCollection("citasMedicas",{
+    validator:{
+        $jsonSchema:{
+            bsonType:"object",
+            required:["fechaCita","doctorId","personId","officeId","diagnosticoClinico","recomendaciones"],
+            properties:{
+                fechaCita:{
+                    bsonType:"date"
+                },
+                diagnosticoClinico:{
+                    bsonType:"string"
+                },
+                recomendaciones:{
+                    bsonType:"string"
+
+                }
+            }
+        }
+    }
+});
+db.createCollection("medicaciones",{
+    validator:{
+        $jsonSchema:{
+            bsonType:"object",
+            required:["citasMedicasId","medicinasId","dosis"],
+            properties:{
+                dosis:{
+                    bsonType:"int",
+                },
+                notas:{
+                    bsonType:"string",
+                    
+
+                }
+            }
+        }
+    }
+})
+
